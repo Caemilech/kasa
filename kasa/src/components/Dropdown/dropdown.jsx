f9d8587a68fa72bdb7b2a974598082ca2132c091
Nicolas Morandi <nicolas.morandi@orange.fr>
2023-12-08 19:38:09 +0100

    Fin page A Propos

--- kasa/src/components/Dropdown/dropdown.jsx
+++ kasa/src/components/Dropdown/dropdown.jsx
@@ -1,16 +1,36 @@
+import { useState } from 'react'
 import { ReactComponent as ArrowDropdown } from './ArrowDropdown.svg'
+import { ReactComponent as ArrowDropdownBack } from './ArrowDropdownBack.svg'
 
-const dropdown = () => {
-   
+const Dropdown = (props) => {
+
+const [isOpen, setIsOpen] = useState(false)
+const dropdownElements = props.dropdownElements
     return (
+        
+        dropdownElements && dropdownElements.map( dropdownElement => {
 
-        <div className='dropdown__container'>
-            
-            <p></p>
-            <ArrowDropdown className='dropdown__arrow'/>
-        </div>
+            return (
 
+                <div key={dropdownElement.id}>
+                    <div className='dropdown__container'>
+                        <p className='dropdown__title'>{dropdownElement.title}</p>
+                        {!isOpen ? (
+                            <ArrowDropdown className='dropdown__arrow' onClick={() => setIsOpen((prev) => !prev)}/>
+                        ):
+                        (
+                            <ArrowDropdownBack className='dropdown__arrow' onClick={() => setIsOpen((prev) => !prev)}/>
+                        )}
+                    </div>
+                    {isOpen && (
+                        <div className='dropdown__text'>
+                            <p>{dropdownElement.text}</p>
+                        </div>
+                    )}
+                </div>        
+            )
+        })  
     )
 }
 
-export default dropdown
+export default Dropdown
--- kasa/src/pages/APropos/aPropos.jsx
+++ kasa/src/pages/APropos/aPropos.jsx
@@ -1,20 +1,49 @@
+import { useState } from 'react'
 import BackgroundAPropos from './BackgroundAPropos.jpg'
 import Dropdown from '../../components/Dropdown/dropdown'
 
-const name = ['Fiabilité']
-
-const aPropos = () => {
+const APropos = () => {
+    const [dropdownElements1] = useState([
+        { 
+            title: 'Fiabilité', 
+            text: 'Les annonces postées sur Kasa garantissant une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifièes par nos équipes.',
+            id: 1
+        }
+    ])
+    const [dropdownElements2] = useState([
+        { 
+            title: 'Respect', 
+            text: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme.', 
+            id: 2
+        }
+    ])
+    const [dropdownElements3] = useState([
+        { 
+            title: 'Service', 
+            text: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme.',
+            id: 3
+        }
+    ])
+    const [dropdownElements4] = useState([
+        { 
+            title: 'Sécurité', 
+            text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour  nos hôtes.",
+            id: 4
+        }
+    ])
     return (
         <div className='aPropos'>
             <div className='background__aPropos-container'>
                 <img className='background__aPropos-img' src={BackgroundAPropos} alt='BackgroundAPropos' />
             </div>
-            <div>
-                <Dropdown />
-                <p className='dropdown__text'>Fiabilité</p>
+            <div className='dropdown__aPropos-container'>
+                <Dropdown dropdownElements={dropdownElements1} />
+                <Dropdown dropdownElements={dropdownElements2} />
+                <Dropdown dropdownElements={dropdownElements3} />
+                <Dropdown dropdownElements={dropdownElements4} />
             </div>
         </div>
     )
 }
 
-export default aPropos
+export default APropos
